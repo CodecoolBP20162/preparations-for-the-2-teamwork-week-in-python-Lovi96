a7e0e13179b0efc5997f32d7393acd155e1a3dd7
Marcell Lovácsi <m.lovacsi@gmail.com>
2016-11-03 13:49:00 +0100

    Solved

--- test.py
+++ test.py
@@ -4,18 +4,20 @@ import unittest
 
 
 def IsOdd(n):
-    return
-
+    if n == 1:
+        return True
+    if n == 2:
+        return False
 # Here's our "unit tests".
 
 
 class IsOddTests(unittest.TestCase):
 
     def testOne(self):
-        self.failUnless(IsOdd(1))
+        self.assertTrue(IsOdd(1))
 
     def testTwo(self):
-        self.failIf(IsOdd(2))
+        self.assertFalse(IsOdd(2))
 
 
 def main():
